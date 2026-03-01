903cf6167a210e857d249f8f32c9fc6d78f873cd
Neha Saini <145911161+neha23nov@users.noreply.github.com>
2026-03-01 16:45:56 +0530

    update

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,15 +2,13 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-
-    await mongoose.connect("mongodb+srv://nehasaini23nov:codemax@cluster1.tjyhhdq.mongodb.net/codeIDE?retryWrites=true&w=majority&appName=Cluster1",{
+    await mongoose.connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
     console.log("MongoDB connected");
-    
   } catch (error) {
-    console.log(error)
+    console.log(error);
   }
 };
 
